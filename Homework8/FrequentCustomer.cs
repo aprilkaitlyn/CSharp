0b57c74e610f8e88d80b5bf61be42eb7ff19ae8d
kaitlyn h <59969864+fpeclique@users.noreply.github.com>
2020-11-09 22:36:01 -0600

    Create FrequentCustomer.cs

---
+++ Homework8/FrequentCustomer.cs
@@ -0,0 +1,20 @@
+namespace Hwk8Heishman
+{
+    public class FrequentCustomer : Customer
+    {
+        private double discountRate;
+
+        public double DiscountRate
+        {
+            get
+            {
+                return discountRate;
+            }
+            set
+            {
+                discountRate = value;
+            }
+        }
+
+    }
+}
